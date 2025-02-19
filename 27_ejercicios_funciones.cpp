0a7b172b7175f4162ede487df974659dad4958dc
gjparts <gjparts@gmail.com>
2025-02-19 11:52:58 -0600

    commit 1902202530567

---
+++ 27_ejercicios_funciones.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "catalogo.h"
+using namespace std;
+
+int main(){
+	
+	
+	
+	return 34567;
+}
---
+++ catalogo.h
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+/*1. Escriba una funcion llamada elevar que reciba dos numeros enteros,
+	uno para la base y otro para la potencia, la funcion debe retornar un entero
+	resultado de elevar el numero base a la potencia sin utilizar la funcion pow.
+	-> la potencia no puede ser un numero negativo. (guard clause)*/
+//si desea soportar enteros mas grandes que int, recomiendo usar long double*/
+
