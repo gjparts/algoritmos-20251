d8d1739a453c484c78c5b4706c4b8a58e9a92a08
gjparts <gjparts@gmail.com>
2025-02-20 11:48:42 -0600

    commit 2002202516961

--- 27_ejercicios_funciones.cpp
+++ 27_ejercicios_funciones.cpp
@@ -3,8 +3,32 @@
 using namespace std;
 
 int main(){
+	cout << elevar(2,4) << endl;
+	cout << elevar(3,8) << endl;
+	cout << elevar(3,0) << endl;
+	cout << elevar(3,1) << endl;
+	//cout << elevar(3,-1) << endl;
 	
+	cout << "Hipotenusa ******" << endl;
+	cout << hipotenusa(4,5) << endl;
+	cout << hipotenusa(10.5,22.3) << endl;
 	
+	cout << "es multiplo ******" << endl;
+	cout << esMultiplo(4,2) << endl;
+	cout << esMultiplo(5,3) << endl;
+	cout << esMultiplo(200,10) << endl;
+	//recuerde que en cualquier lenguaje de programacion
+	//si Usted imprime una expresion booleana
+	//lo que se devolvera sera true o false (1 o 0)
+	cout << ( 6 > 2 ) << endl;
+	cout << ( 6 > 2 && 4 == 4 ) << endl;
+	cout << ( 7 != 7 ) << endl;
+	//tambien recuerde que true suma 1, false suma 0:
+	int algo = 0;
+	algo += ( 5 > 3 );
+	algo += ( 8 > 4 );
+	algo += ( 20 > 100 );
+	cout << "algo: " << algo << endl;
 	
 	return 34567;
 }
--- catalogo.h
+++ catalogo.h
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<stdexcept>
+#include<cmath>
 using namespace std;
 
 /*1. Escriba una funcion llamada elevar que reciba dos numeros enteros,
@@ -6,4 +8,51 @@ using namespace std;
 	resultado de elevar el numero base a la potencia sin utilizar la funcion pow.
 	-> la potencia no puede ser un numero negativo. (guard clause)*/
 //si desea soportar enteros mas grandes que int, recomiendo usar long double*/
+long double elevar(long double base, long double potencia){
+	if( potencia < 0 )
+		throw invalid_argument("potencia no puede ser negativa"); //guard clause
+	
+	long double tmp = 1;
+	for( int i = 1; i <= potencia; i++ ){
+		tmp = tmp * base;
+	}
+	return tmp; //retornar o devolver el resultado
+}
+/*2. Defina una funcion llamada hipotenusa que retorne
+la longitud de la hipotenusa de un triángulo rectángulo,
+la funcion recibira los lados de dicho triangulo.
+La funcion debera funcionar con numeros double, se le permite usar sqrt() y pow().*/
+double hipotenusa( double opuesto, double adyacente ){
+	return sqrt( pow(opuesto,2)+pow(adyacente,2) );
+}
+/*3. Escriba una funcion que reciba dos numeros enteros y retorne true
+si el primer numero es multiplo del segundo o false en caso contrario.*/
+bool esMultiplo( int a, int b ){
+	//forma 1:
+	/*if( a%b == 0 )
+		return true;
+	else
+		return false;*/
+	//forma 2:
+	return ( a%b == 0 );
+	//forma 3: usando el operador ternario
+	//return ( a%b == 0 ? true : false );
+}
+/*4. Escriba una funcion que imprima un rectagulo de caracteres,
+dicha funcion recibira como parametros el ancho y largo del mismo
+asi como otro parametro con el caracter a usar para dibujar
+dicho rectangulo.*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
