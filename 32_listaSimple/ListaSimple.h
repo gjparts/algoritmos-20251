3ad35d404a9cf15c7015bcb8d81508951ce273d5
gjparts <gjparts@gmail.com>
2025-03-31 11:54:50 -0600

    commit 3103202511808

--- 32_listaSimple/ListaSimple.h
+++ 32_listaSimple/ListaSimple.h
@@ -18,6 +18,54 @@ class ListaSimple{
 			this->last = NULL;
 			this->length = 0; //no tiene Nodos aun
 		}
+		//metodos
+		Nodo *getFirst(){
+			return this->first;
+		}
+		Nodo *getLast(){
+			return this->last;
+		}
+		int getLength(){
+			return this->length;
+		}
+		//metodo para agregar un nuevo Nodo al final de la lista
+		void push(string value){
+			//1) crear un nuevo Nodo llamado tmp cuyo valor sea value
+			Nodo *tmp = new Nodo(value);
+			/*2) Si length es Cero entonces:
+				a) first y last apuntaran al Nodo tmp
+			sino
+				a) el next del Nodo last apuntara al Nodo tmp
+				b) el Nodo last apuntara a tmp*/
+			if( this->length == 0 ){
+				this->first = tmp;
+				this->last = tmp;
+			}
+			else{
+				this->last->next = tmp;
+				this->last = tmp;
+			}
+			//3) sumar 1 a length
+			this->length++;
+		}
+		//metodo para imprimir el contenido de la lista en pantalla
+		void print(){
+			//Algoritmo de recorrido
+			//1) Declarar variable tmp de tipo Nodo y apuntarla a first
+			Nodo *tmp = this->first;
+			/*2) Mientras el Nodo tmp sea distinto de NULL entonces:
+					a) imprimir el value de tmp
+					b) apuntar tmp al next de tmp (pasar al siguiente Nodo
+				Sino:
+					a) terminar el recorrido*/
+			while( tmp != NULL ){
+				cout << tmp->value << "->";
+				tmp = tmp->next; //pasar al siguiente Nodo
+			}
+			//se termino el recorrido
+			cout << "NULL" << endl; //imprimo el letrero NULL
+		}
+		
 };
 
 #endif
--- 32_listaSimple/main.cpp
+++ 32_listaSimple/main.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
+#include "Nodo.h"
+#include "ListaSimple.h"
 using namespace std;
 
 int main(){
+	//crear un nuevo objeto de clase ListaSimple
+	ListaSimple *lista1 = new ListaSimple();
+	//prueba de metodos
+	lista1->push("5");
+	lista1->push("7");
+	lista1->push("1");
+	lista1->push("40");
+	lista1->push("12");
+	//longitud de la lista
+	cout << "Nodos en lista1: " << lista1->getLength() << endl;
+	//valores del primer y ultimo Nodo
+	cout << "Valor del primer Nodo en lista1: " << lista1->getFirst()->value << endl;
+	cout << "Valor del ultimo Nodo en lista1: " << lista1->getLast()->value << endl;
 	
+	lista1->print();
 	
 	return 777;
 }
